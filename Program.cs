2af8d7e225d8597779f499d0e7997068888b329e
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-11-08 11:06:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uppfit1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            int slumptal = random.Next(1, 101);
+            int antalFörsök = 0;
+
+            while (true)
+            {
+                Console.Write("Gissa ett tal mellan 1 och 100: ");
+                int användarensGissning = int.Parse(Console.ReadLine());
+                antalFörsök++;
+
+                if (användarensGissning == slumptal)
+                {
+                    Console.WriteLine("Rätt tal! Det tog " + antalFörsök + " försök att gissa rätt.");
+                    break;
+                }
+                else if (användarensGissning < slumptal)
+                {
+                    Console.WriteLine("Gissningen är för låg.");
+                }
+                else
+                {
+                    Console.WriteLine("Gissningen är för hög.");
+                }
+            }
+        }
+    }
+}
+
